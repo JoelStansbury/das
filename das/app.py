28e17b5f62d6700af4c919c1303e64720eb8a512
varyee <ayelearyee@icloud.com>
2022-10-10 22:14:49 -0400

    added get inbox route

--- das/app.py
+++ das/app.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from flask import Flask, render_template
+from flask import Flask, render_template,send_from_directory
 from flask_cors import CORS
 
 HERE = Path(__file__).parent
@@ -20,10 +20,14 @@ def index():
 def main():
     return render_template("main.html", python_variable="Success")
 
+@app.get("/api/<username>/inbox")
+def get_inbox(username):
+    return send_from_directory(STATIC, "mock/example_inbox.json")
+
 @app.post("/api/encrypt/<path>")
 def encrypt(path):
     return f"I don't know how to do this yet: {path}"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run( )
