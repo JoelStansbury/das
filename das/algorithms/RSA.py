646696a4ce456c169737f5ae0cdde817c169682c
JoelStansbury <stansbury.joel@gmail.com>
2022-10-12 23:37:23 -0400

    formatting

--- das/algorithms/RSA.py
+++ das/algorithms/RSA.py
@@ -9,15 +9,14 @@ class RSA:
     def __init__(self, p=None, q=None):
         self.p = p or random.choice(LARGE_PRIMES)
         self.q = q or random.choice(LARGE_PRIMES)
-        self.n = self.p*self.q
-        self.phi_n = (self.p-1)*(self.q-1)
+        self.n = self.p * self.q
+        self.phi_n = (self.p - 1) * (self.q - 1)
 
         for k in range(2, self.phi_n):  # 1 < k < phi_n
             if Algorithms.gcd(k, self.phi_n) == 1:
                 self.k = k
                 break
         self.d = mod(self.phi_n).inverse(self.k)
-
 
     def encrypt(self, message):
         ciphertext = 1
@@ -31,6 +30,7 @@ class RSA:
             message = message * ciphertext % self.n
         return message
 
+
 if __name__ == "__main__":
     r = RSA(79, 89)
 
@@ -38,6 +38,8 @@ if __name__ == "__main__":
     print(f"d = {r.d} (should be 1373)")
     print(f"d = {r.n} (should be 7031)")
 
-    assert r.encrypt(44) == 4119, "Does not match example (p=79, q=89, ciphertext=4119). NOT {r.encrypt(44)}"
+    assert (
+        r.encrypt(44) == 4119
+    ), "Does not match example (p=79, q=89, ciphertext=4119). NOT {r.encrypt(44)}"
     assert r.d * r.k % r.phi_n == 1, "d and k are not inverses!"
     assert r.decrypt(r.encrypt(100)) == 100, r.decrypt(r.encrypt(100))
