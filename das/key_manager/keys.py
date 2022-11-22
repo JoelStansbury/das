17c1906fbc34daf60e0e1d84047d847a78c14688
Lionion <rtdeem@gmail.com>
2022-11-21 22:18:40 -0500

    updated keys.py

--- das/key_manager/keys.py
+++ das/key_manager/keys.py
@@ -2,25 +2,41 @@
 import csv
 import os
 from pathlib import Path
+from random import random
 
-def get_private_aes_key(my_email_address):
+
+def get_private_rsa_key(my_email_address):
     pass
 
-def get_public_aes_key(my_email_address):
+
+def get_public_rsa_key(my_email_address):
     pass
+
 
 def get_3des_key(other_user):
     # return key1, key2, key3
     pass
 
+
 def save_3des_key(other_user, key1, key2, key3):
+    save_keys("", "", key1, key2, key3, other_user)
+
+
+def generate_rsa_key(my_email_address):
     pass
 
-def generate_aes_key(my_email_address):
-    pass
 
-def generate_des_key(my_email_address):
-    pass
+def generate_des_key(other_user):
+    s = []
+    for i in range(3):
+        s.append("")
+        for j in range(64):
+            if random() > .5:
+                s[i] += "1"
+            else:
+                s[i] += "0"
+    save_3des_key(other_user, s[0], s[1], s[2])
+
 
 def save_keys(rsa_pubkey, rsa_prikey, des_key_1, des_key_2, des_key_3, sender_name):
     keys = []
